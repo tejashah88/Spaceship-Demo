62fc5619d5e3d900f8240c169df6b14f28404cde
Tejas Shah <tejashah88@gmail.com>
2020-05-13 23:26:04 -0700

    updated to 2020 (although camera [s]lerp is broken)

--- Assets/MyAssets/scripts/BeamBoltsController.cs
+++ Assets/MyAssets/scripts/BeamBoltsController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class BeamBoltsController : MonoBehaviour {
 
-  public bool shouldCutOff = false;
-  public BeamParam left;
-  public BeamParam right;
+    public bool shouldCutOff = false;
+    public BeamParam left;
+    public BeamParam right;
 
-	// Update is called once per frame
-	void Update () {
-    left.bEnd = shouldCutOff;
-    right.bEnd = shouldCutOff;
-	}
+// Update is called once per frame
+    void Update () {
+        left.bEnd = shouldCutOff;
+        right.bEnd = shouldCutOff;
+    }
 }
--- Assets/MyAssets/scripts/SmoothFollow2.cs
+++ Assets/MyAssets/scripts/SmoothFollow2.cs
@@ -1,35 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 public class SmoothFollow2 : MonoBehaviour {
-  public Transform target;
-  public float distance = 3.0f;
-  public float height = 3.0f;
-  public float damping = 5.0f;
-  public bool smoothRotation = true;
-  public bool followBehind = true;
-  public float rotationDamping = 10.0f;
-  public float rotationLookOffset;
+    public Transform target;
+    public float distance = 3.0f;
+    public float height = 3.0f;
+    public float damping = 5.0f;
+    public bool smoothRotation = true;
+    public bool followBehind = true;
+    public float rotationDamping = 10.0f;
+    public float rotationLookOffset;
+    public float smoothTime = 0.3f;
 
-  public bool shouldRotate;
+    private Vector3 velocity = Vector3.zero;
 
-  void Update () {
-    Vector3 wantedPosition;
-    if (followBehind)
-      wantedPosition = target.TransformPoint(0, height, -distance);
-    else
-      wantedPosition = target.TransformPoint(0, height, distance);
+    void FixedUpdate() {
+        Vector3 wantedPosition;
+        if (followBehind)
+            wantedPosition = target.TransformPoint(0, height, -distance);
+        else
+            wantedPosition = target.TransformPoint(0, height, distance);
 
-    transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * damping);
+        // transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * damping);
+        transform.position = Vector3.SmoothDamp(transform.position, wantedPosition, ref velocity, smoothTime);
 
-    if (smoothRotation) {
-      Quaternion wantedRotation = Quaternion.LookRotation(
-        target.position - transform.position + Vector3.Scale(
-          Vector3.one * rotationLookOffset,
-          target.transform.forward
-        ),
-        target.up
-      );
-      transform.rotation = Quaternion.Slerp(transform.rotation, wantedRotation, Time.deltaTime * rotationDamping);
-    } else transform.LookAt(target, target.up);
-  }
+        if (smoothRotation) {
+            Quaternion wantedRotation = Quaternion.LookRotation(
+                target.position - transform.position + Vector3.Scale(
+                    Vector3.one * rotationLookOffset,
+                    target.transform.forward
+                ),
+                target.up
+            );
+            transform.rotation = Quaternion.Slerp(transform.rotation, wantedRotation, Time.deltaTime * rotationDamping);
+        } else
+            transform.LookAt(target, target.up);
+    }
 }
